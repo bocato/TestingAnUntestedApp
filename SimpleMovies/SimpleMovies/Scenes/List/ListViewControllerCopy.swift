c385de38d27739727e9195c9a4be29fa18c8d6a7
André Sanches Bocato <andrebocato@gmail.com>
2020-09-21 22:28:15 -0300

    Duplicated ListViewController and added some non-sense code changes to generate a fake PR and fake code review

---
+++ SimpleMovies/SimpleMovies/Scenes/List/ListViewControllerCopy.swift
@@ -0,0 +1,9 @@
+//
+//  ListViewControllerCopy.swift
+//  SimpleMovies
+//
+//  Created by André Sanches Bocato on 21/09/20.
+//  Copyright © 2020 Eduardo Sanches Bocato. All rights reserved.
+//
+
+import Foundation
